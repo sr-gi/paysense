d4145a937252abaca434e8721ad1b41212a3e4d0
sr-gi <sergi.delgado.s@gmail.com>
2015-07-06 11:36:09 +0200

    Comments added to bitcointransactions

--- bitcointransactions.py
+++ bitcointransactions.py
@@ -3,7 +3,15 @@ from M2Crypto import EC
 from bitcointools import *
 from bitcoin import *
 
-def single_payment(s_key, own_bc_address, cs_bc_address, amount, outside_bc_address, outside_amount, fee=None):
+# Performs a bitcoin transaction from a single user (inputs to be signed just by one private key)
+# @s_key is the OpenSSl private key object representing the elliptic curve private key
+# @source_bc_address is the CS own bitcoin address, where the bitcoins came from
+# @destination_bc_address is the bitcoin address of the new CS pseudonym, where the bitcoins will be transferred
+# @amount is the bitcoin amount to be transferred from the source to the destination bitcoin addresses
+# @outside_bc_address is the bitcoin address where the withdrawal amount of bitcoin will go
+# @outside_amount is the amount of bitcoin that will be withdrawn
+# @fee represent the transaction fee, it is set to 1000 Satoshi by default
+def single_payment(s_key, source_bc_address, destination_bc_address, amount, outside_bc_address, outside_amount, fee=None):
     # Set the default fee
     if fee is None:
         fee = 1000
@@ -12,10 +20,10 @@ def single_payment(s_key, own_bc_address, cs_bc_address, amount, outside_bc_addr
     private_key_hex = get_priv_key_hex(s_key)
 
     # Check the unspent bitcoins from that address
-    unspent_bitcoins = blockr_unspent(own_bc_address, 'testnet')
+    unspent_bitcoins = blockr_unspent(source_bc_address, 'testnet')
 
     # Build the output of the payment
-    outs = [{'value': amount - outside_amount - fee, 'address': cs_bc_address}, {'value': outside_amount, 'address': outside_bc_address}]
+    outs = [{'value': amount - outside_amount - fee, 'address': destination_bc_address}, {'value': outside_amount, 'address': outside_bc_address}]
 
     # Build the transaction
     tx = mktx(unspent_bitcoins, outs)
@@ -27,7 +35,9 @@ def single_payment(s_key, own_bc_address, cs_bc_address, amount, outside_bc_addr
 
     print result
 
-
+# Performs a bitcoin transaction from a multiple users (inputs to be signed by more than one private key)
+# ToDo: CHANGE THIS FUNCTION
+# Currently is mostly hardcoded, maybe it will be deleted. If it's not, it will be improved.
 def multi_payment(cs1_p_key, cs1_s_key, cs2_p_key, cs2_s_key, cs_bitcoin_address, amount, fee=None):
     if fee is None:
         fee = 1000
